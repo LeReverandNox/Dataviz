8ea4c084005d686bfea7d68a372b24c07c0dcf7d
laidet_r <rodolphe.laidet@epitech.eu>
2016-07-20 17:07:35 +0200

    Création des 4 controllers pour les 4 tabs. Avec juste un attribut title et un console.log pour le moment.

--- js/controllers.js
+++ js/controllers.js
@@ -5,4 +5,24 @@
     "use strict";
 
     var controllers = angular.module("dataviz-controllers", []);
+
+    controllers.controller("HomeCtrl", function () {
+        console.log("HomeCtrl");
+    });
+    controllers.controller("amChartsCtrl", function () {
+        this.title = "amCharts";
+        console.log("amChartsCtrl");
+    });
+    controllers.controller("highChartsCtrl", function () {
+        this.title = "HighCharts";
+        console.log("highchartsCtrl");
+    });
+    controllers.controller("D3JSCtrl", function () {
+        this.title = "D3JS";
+        console.log("D3JSCtrl");
+    });
+    controllers.controller("RaphaelJSCtrl", function () {
+        this.title = "RaphaelJS";
+        console.log("RaphaelJSCtrl");
+    });
 }());
