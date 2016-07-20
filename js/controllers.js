71338d51646179f1880f6562aa98ed0913394103
laidet_r <rodolphe.laidet@epitech.eu>
2016-07-21 00:34:43 +0200

    GROSSE méthode de génération de graph avec D3JS v3.... pfiou paye ta prise de tête, merci les tutos. Ca fonctionne !

--- js/controllers.js
+++ js/controllers.js
@@ -353,6 +353,71 @@
             }
         };
 
+        this.generateChart = function (data) {
+            this.deleteOldGraph();
+
+            var max = d3.max(data, function(d) { return d.count; });
+            var margin = {top: 20, right: 20, bottom: 50, left: 40},
+                width = 960 - margin.left - margin.right,
+                height = 500 - margin.top - margin.bottom;
+
+            var x = d3.scale.ordinal()
+                .rangeRoundBands([0, width], .1);
+
+            var y = d3.scale.linear()
+                .range([height, 0]);
+
+            var xAxis = d3.svg.axis()
+                .scale(x)
+                .orient("bottom");
+
+            var yAxis = d3.svg.axis()
+                .scale(y)
+                .orient("left")
+                .ticks(max);
+
+            var svg = d3.select("#d3js").append("svg")
+                .attr("class", "chart")
+                .attr("width", width + margin.left + margin.right)
+                .attr("height", height + margin.top + margin.bottom)
+              .append("g")
+                .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+
+              x.domain(data.map(function(d) { return d.dep; }));
+              y.domain([0, max]);
+
+              svg.append("g")
+                  .attr("class", "x axis")
+                  .attr("transform", "translate(0," + height + ")")
+                  .call(xAxis)
+                .append("text")
+                  .attr("y", 35)
+                  .attr("x", width / 2)
+                  .attr("font-size", "1.5em")
+                  .style("text-anchor", "middle")
+                  .text("Départements");
+
+              svg.append("g")
+                  .attr("class", "y axis")
+                  .call(yAxis)
+                .append("text")
+                  .attr("transform", "rotate(-90)")
+                  .attr("y", -25)
+                  .attr("x", -(height / 2))
+                  .attr("font-size", "1.5em")
+                  .style("text-anchor", "middle")
+                  .text("Nombre");
+
+              svg.selectAll(".bar")
+                  .data(data)
+                .enter().append("rect")
+                  .attr("class", "bar")
+                  .attr("x", function(d) { return x(d.dep); })
+                  .attr("width", x.rangeBand())
+                  .attr("y", function(d) { return y(d.count); })
+                  .attr("height", function(d) { return height - y(d.count); });
+        };
     });
 
     controllers.controller("RaphaelJSCtrl", function () {
