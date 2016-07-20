af1ae88dca087a7ab7eb7decbbf8fab927caefb9
laidet_r <rodolphe.laidet@epitech.eu>
2016-07-20 17:13:50 +0200

    Écriture des 2 premieres méthodes du controller amChartsCtrl, qui traite les données recues afin d'etre utilisées dans le graph.

--- js/controllers.js
+++ js/controllers.js
@@ -9,10 +9,46 @@
     controllers.controller("HomeCtrl", function () {
         console.log("HomeCtrl");
     });
-    controllers.controller("amChartsCtrl", function () {
+
+    controllers.controller("amChartsCtrl", function (DataService) {
+        var self = this;
         this.title = "amCharts";
-        console.log("amChartsCtrl");
+
+        DataService.getData(function (data) {
+            console.log(data);
+            // var formatedDatas = self.proceedData(data);
+            // self.generateChart(formatedDatas);
+        });
+
+        this.proceedData = function (data) {
+            var formatedDatas = [];
+            data.forEach(function (record) {
+                var dep = record.fields.adresse_administrative_code_departement_du_tiers_beneficiaire;
+                if (dep === undefined) {
+                    return;
+                }
+                var obj = {};
+                var existing = self.isDepInArray(formatedDatas, dep);
+                if (!existing) {
+                    obj.departement = dep;
+                    obj.count = 1;
+                    formatedDatas.push(obj);
+                } else {
+                    existing.count += 1;
+                }
+            });
+            return formatedDatas;
+        };
+        this.isDepInArray = function (formatedDatas, dep) {
+            var arr = formatedDatas.filter(function (data) {
+                return data.departement === dep;
+            });
+            return (arr.length === 0)
+                ? false
+                : arr[0];
+        };
     });
+
     controllers.controller("highChartsCtrl", function () {
         this.title = "HighCharts";
         console.log("highchartsCtrl");
