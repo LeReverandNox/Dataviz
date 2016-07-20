158d3cffd121cacab0dc63f1edb9146ccbcb597b
laidet_r <rodolphe.laidet@epitech.eu>
2016-07-20 17:12:08 +0200

    Création du service DataService qui récupère les données depuis l'url du sujet et les renvoie a la callback passée en parametre

--- js/services.js
+++ js/services.js
@@ -5,4 +5,14 @@
     "use strict";
 
     var services = angular.module("dataviz-services", []);
+
+    services.service("DataService", function ($http) {
+        this.url = "http://data.iledefrance.fr/explore/dataset/aide-aux-festivals-de-musiques-actuelles-et-amplifiees-2011-2013/download?format=json";
+        this.getData = function (successCallback) {
+            $http.jsonp(this.url + "&callback=JSON_CALLBACK")
+                .then(function (resp) {
+                    successCallback(resp.data);
+                });
+        };
+    });
 }());
