c83efaa1d8b525c1388b9167a509f55decc2370f
laidet_r <rodolphe.laidet@epitech.eu>
2017-03-29 10:37:40 +0200

    Now using https to call the API

--- js/services.js
+++ js/services.js
@@ -7,7 +7,7 @@
     var services = angular.module("dataviz-services", []);
 
     services.service("DataService", function ($http) {
-        this.url = "http://data.iledefrance.fr/explore/dataset/aide-aux-festivals-de-musiques-actuelles-et-amplifiees-2011-2013/download?format=json";
+        this.url = "https://data.iledefrance.fr/explore/dataset/aide-aux-festivals-de-musiques-actuelles-et-amplifiees-2011-2013/download?format=json";
         this.getData = function (successCallback) {
             $http.jsonp(this.url + "&callback=JSON_CALLBACK")
                 .then(function (resp) {
